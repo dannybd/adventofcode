25bba285d786610ec983f4271329a642e77db293
Daniel M Ben-David <dannybd@mit.edu>
2020-12-04 00:47:27 -0500

    Rewrite 4b to write regex more legibly

--- 2020/4b.js
+++ 2020/4b.js
@@ -1,5 +1,12 @@
-document.body.innerText.trim().split('\n\n')
-  .filter(p => p.match(
-      /\b(byr:(19[2-9]\d|200[012])|iyr:(201\d|2020)|eyr:(202\d|2030)|hgt:(1[5678]\dcm|19[0123]cm|59in|6\din|7[0-6]in)|hcl:#[0-9a-f]{6}|ecl:(amb|blu|brn|gry|grn|hzl|oth)|pid:\d{9})\b/g
-    )?.length === 7
-  ).length
+(regex => document.body.innerText.trim().split('\n\n')
+  .filter(p => p.match(new RegExp(regex, 'g'))?.length === 7)
+  .length
+)(`\\b(
+  byr:(19[2-9]\\d|200[012])|
+  iyr:(201\\d|2020)|
+  eyr:(202\\d|2030)|
+  hgt:((1[5678]\\d|19[0123])cm|(59|6\\d|7[0-6])in)|
+  hcl:#[0-9a-f]{6}|
+  ecl:(amb|blu|brn|gry|grn|hzl|oth)|
+  pid:\\d{9}
+)\\b`.replace(/\s/g,''))
