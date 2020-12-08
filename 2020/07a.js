f7d4d85efbd72adcf19ea0be80a1bff762159e0c
Daniel M Ben-David <dannybd@mit.edu>
2020-12-07 21:57:16 -0500

    Day 7 but this time with Y-combinators. TIL!

--- 2020/07a.js
+++ 2020/07a.js
@@ -1,11 +1,22 @@
-const bags = Object.fromEntries(
-  document.body.innerText.trim()
-    .replace(/no other bag/g, '')
-    .split('\n')
-    .map(x => x.match(/\w+ \w+(?= bag)/g))
-    .map(x => [x[0], x.slice(1)])
-);
-function holdsShiny(b) {
-  return bags[b].some(v => v === 'shiny gold' || holdsShiny(v));
-}
-Object.keys(bags).filter(b => holdsShiny(b)).length;
+(
+  (bags, y) => Object.keys(bags)
+    .filter(bagToCheck => y(
+      holdsShiny => bag => bags[bag].some(
+        subbag => subbag === 'shiny gold' || holdsShiny(subbag)
+      ),
+      bagToCheck
+    ))
+    .length
+)(
+  // Map of bags to list of sub bags
+  Object.fromEntries(
+    document.body.innerText.trim()
+      .replace(/no other bag/g, '')
+      .split('\n')
+      .map(x => x.match(/\w+ \w+(?= bag)/g))
+      .map(x => [x[0], x.slice(1)])
+  ),
+  // Y combinator, allowing recursive lambda g to be called w/ initial value z
+  // Credit: http://kestas.kuliukas.com/YCombinatorExplained/
+  (g, z) => (y => y(g)(z))(le => (f => f(f))(f => le(x => (f(f))(x))))
+)
--- 2020/07b.js
+++ 2020/07b.js
@@ -1,20 +1,31 @@
-const bags = Object.fromEntries(
-  document.body.innerText.trim()
-    .replace(/no other bag/g, '')
-    .split('\n')
-    .map(x => x.match(/(\d+ )?\w+ \w+(?= bag)/g))
-    .map(x => [
-      x[0],
-      x.slice(1).map(y =>
-        (
-          z => ({count: z[0] - 0, type: z.slice(1).join(' ')})
-        )(y.split(' '))
-      )
-    ])
-);
-function numBags(b) {
-  return bags[b.type]
-    .map(v => numBags({type: v.type, count: v.count * b.count}))
-    .reduce((a, b) => a + b, b.count);
-}
-numBags({type: 'shiny gold', count: 1}) - 1;
+(
+  (bags, y) => y(
+      numBags => bag => bags[bag.type]
+        .map(subbag => numBags(
+          {type: subbag.type, count: subbag.count * bag.count}
+        ))
+        .reduce((a, b) => a + b, bag.count),
+      {type: 'shiny gold', count: 1}
+    ) - 1
+)(
+  // Map of bags to list of list of bags with their counts
+  Object.fromEntries(
+    document.body.innerText.trim()
+      .replace(/no other bag/g, '')
+      .split('\n')
+      .map(line => line.match(/(\d+ )?\w+ \w+(?= bag)/g))
+      .map(bagGroup => [
+        bagGroup[0],
+        bagGroup.slice(1).map(bagData =>
+          (
+            parts => ({count: parts[0] - 0, type: parts.slice(1).join(' ')})
+          )(
+            bagData.split(' ')
+          )
+        )
+      ])
+  ),
+  // Y combinator, allowing recursive lambda g to be called w/ initial value z
+  // Credit: http://kestas.kuliukas.com/YCombinatorExplained/
+  (g, z) => (y => y(g)(z))(le => (f => f(f))(f => le(x => (f(f))(x))))
+)
