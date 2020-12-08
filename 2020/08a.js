e5403770d5a04802f0620926d53e6cea4e7723d6
Daniel M Ben-David <dannybd@mit.edu>
2020-12-08 01:15:25 -0500

    One-linerize day 8, using .reduce in a slightly weird way

--- 2020/08a.js
+++ 2020/08a.js
@@ -1,20 +1,21 @@
-const lines = document.body.innerText.trim().split('\n');
-let i = x = 0;
-while (line = lines[i]) {
-  lines[i] = null;
-  [op, val] = line.split(' ');
-  val = val - 0;
-  switch (op) {
-    case 'nop':
-      i++;
-      continue;
-    case 'acc':
-      x += val;
-      i++;
-      continue;
-    case 'jmp':
-      i += val;
-      continue;
-  }
-}
-x
+(lines =>
+  // Provides a loop of instructions in order; since we never run the same
+  // instruction twice, we don't need this loop to be any longer than lines.
+  // The second arc in .reduce() is unused here, because we're stepping
+  // through using values in data itself (starting at its initialized value)
+  lines.reduce((data, _) =>
+    ((instruction, argument) =>
+      data.visited.includes(data.lineNum)
+        ? data
+        : {
+            accumulator:
+              data.accumulator + (instruction === 'acc' ? argument - 0 : 0),
+            lineNum:
+              data.lineNum + (instruction === 'jmp' ? argument - 0 : 1),
+            visited: [...data.visited, data.lineNum]
+          }
+    )(...lines[data.lineNum].split(' ')),
+    {accumulator: 0, lineNum: 0, visited: []}
+  )
+  .accumulator
+)(document.body.innerText.trim().split('\n'))
--- 2020/08b.js
+++ 2020/08b.js
@@ -1,31 +1,33 @@
-const lines = document.body.innerText.trim().split('\n');
-function tryFinish(lines, j) {
-  lines = [...lines];
-  if (!/jmp|nop/.test(lines[j])) {
-    return null;
-  }
-  let x = 0;
-  let i = 0;
-  while (line = lines[i]) {
-    lines[i] = null;
-    [op, val] = line.split(' ');
-    val = val - 0;
-    if (i === j) {
-      op = op === 'nop' ? 'jmp' : 'nop';
-    }
-    switch (op) {
-      case 'nop':
-        i++;
-        continue;
-      case 'acc':
-        x += val;
-        i++;
-        continue;
-      case 'jmp':
-        i += val;
-        continue;
-    }
-  }
-  return i === lines.length ? x : null;
-}
-[...Array(lines.length).keys()].map(j => tryFinish(lines, j)).find(x => x);
+(lines =>
+  // Using the line numbers to test what happens if we flip things
+  lines.map((_, flippedInstLineNum) =>
+    // Provides a loop of instructions in order; since we never run the same
+    // instruction twice, we don't need this loop to be any longer than lines.
+    // The second arc in .reduce() is unused here, because we're stepping
+    // through using values in data itself (starting at its initialized value)
+    lines.reduce((data, _) =>
+      (
+        (instruction, argument) => data.visited.includes(data.lineNum)
+          ? data
+          : {
+              accumulator:
+                data.accumulator + (instruction === 'acc' ? argument - 0 : 0),
+              lineNum: data.lineNum + (
+                // If the lineNum is the one we want to test the flipped
+                // instruction, then we XOR the comparison to jmp
+                (
+                  /jmp|nop/.test(instruction) &&
+                  (data.lineNum === flippedInstLineNum ^ instruction === 'jmp')
+                ) ? argument - 0 : 1
+              ),
+              visited: [...data.visited, data.lineNum]
+            }
+      )(...(lines[data.lineNum] || ' ').split(' ')),
+      // Initial data
+      {accumulator: 0, lineNum: 0, visited: []}
+    )
+  )
+  // The interesting answer is the one where we're past the code's end
+  .find(data => data.lineNum >= lines.length)
+  .accumulator
+)(document.body.innerText.trim().split('\n'))
