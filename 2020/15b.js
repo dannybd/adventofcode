968b69fb844f74a641b698bd59502a76016fa7b9
Daniel M Ben-David <dannybd@mit.edu>
2020-12-15 02:24:48 -0500

    15b: use JS bool logic to avoid expensive copying

--- 2020/15b.js
+++ 2020/15b.js
@@ -1,16 +1,23 @@
 (input =>
   [...Array(3E7).keys()]
     .slice(input.length)
-    .reduce((data, turn) =>
-      ({
-        last: {
-          ...data.last,
-          [data.spoken]: turn
-        },
-        spoken: data.spoken in data.last
-          ? turn - data.last[data.spoken]
-          : 0
-      }),
+    .reduce(
+      (data, turn) =>
+        (spoken =>
+          // This feels like cheating, but otherwise copying data.last
+          // (size ~3.1M by the end) will break JS console's ability to
+          // run the code. Instead, we mutate the provided data object
+          // directly, and chain together using booleans to ensure
+          // the modified data object is returned at the end.
+          (data.last[data.spoken] = turn) !== undefined &&
+          (data.spoken = spoken) !== undefined &&
+          data
+        )
+        (
+          data.spoken in data.last
+            ? turn - data.last[data.spoken]
+            : 0
+        ),
       {
         last: Object.fromEntries(input.map((n, i) => [n, i + 1])),
         spoken: input[input.length - 1]
