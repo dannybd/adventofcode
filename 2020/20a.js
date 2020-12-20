926a70ec66e2fa541932651d5279757a14eb9390
Daniel M Ben-David <dannybd@mit.edu>
2020-12-20 01:28:30 -0500

    Cleaner 20a with comments

--- 2020/20a.js
+++ 2020/20a.js
@@ -1,44 +1,37 @@
 document.body.innerText.trim()
   .split('\n\n')
   .map(tile =>
+    // The info we care about from each tile is its id and
+    // the edges of the grid in both directions
     ((id, grid) =>
       ({
         id,
-        grid,
-        possibleEdges: new Set(
+        edges:
           [
             grid[0],
             grid.map(l => l[grid.length - 1]).join(''),
             grid[grid.length - 1],
             grid.map(l => l[0]).join(''),
           ]
+            // List both directions of the edges
             .flatMap(dir => [dir, dir.split('').reverse().join('')])
-            .map(dir =>
-              parseInt(dir.replace(/./g, c => c === '#' ? 1 : 0), 2)
-            )
-        )
       })
     )(
       tile.match(/\d+/) - 0,
       tile.split('\n').slice(1)
     )
   )
-  .map((tile, _, tiles) =>
-    ({
-      ...tile,
-      possibleNeighbors: tiles
-        .map(neighbor =>
-          ({
-            id: neighbor.id,
-            overlap:
-              16 -
-              (new Set([...tile.possibleEdges, ...neighbor.possibleEdges]))
-                .size
-          })
-        )
-        .filter(neighbor => neighbor.overlap)
-    })
+  .filter((tile, _, tiles) =>
+    tiles
+      .filter(neighbor =>
+        // If the tile has edges in common with the neighbor,
+        // this set will have overlap, and the set size will be
+        // less than 8 + 8 = 16
+        (new Set([...tile.edges, ...neighbor.edges])).size < 16
+      )
+      .length
+      // Why 3? Because we checked the tile against itself, too
+      === 3
   )
-  .filter(tile => tile.possibleNeighbors.length <= 3)
-  .map(tile => tile.id)
-  .reduce((a, b) => a * b)
+  // Take the product of the IDs
+  .reduce((a, b) => a * b.id, 1)
