9a57fcd618a5567f1905ee735b55bfa251c5729c
Daniel M Ben-David <dannybd@mit.edu>
2020-12-25 03:30:45 -0500

    Updated 23a to linkedlist

--- 2020/23a.js
+++ 2020/23a.js
@@ -1,43 +1,59 @@
-((cups, rounds) =>
-  ((rotateLeft) =>
+((cupsList, rounds) =>
+  (linkedCups =>
     (cupsAfterGame =>
-      [...Array(cupsAfterGame.indexOf(1))]
-        .reduce(cups => rotateLeft(cups), cupsAfterGame)
+      [...Array(cupsList.length - 1)]
+        .reduce(
+          cups =>
+            cups.push(cupsAfterGame[cups.slice(-1)[0]]) && cups,
+          [1]
+        )
         .slice(1)
         .join('')
     )(
       [...Array(rounds).keys()]
-        .reduce((cups, round) =>
-          ((currentCup, pickUpCups, remainingCups) =>
-            rotateLeft(
-              remainingCups
-                .splice(
-                  1 + remainingCups.indexOf(
-                    [...remainingCups]
-                      .sort((a, b) =>
-                        (f =>
-                          f(b) - f(a)
-                        )(
-                          n => n + 100 * (n < currentCup)
-                        )
-                      )
-                      [0]
-                  ),
-                  0,
-                  ...pickUpCups,
-                ) &&
-              remainingCups
-            )
+        .reduce(({cups, current}) =>
+          (newCups =>
+            ({
+              cups: newCups,
+              current: newCups[current],
+            })
           )(
-            cups[0],
-            cups.slice(1, 4),
-            [cups[0], ...cups.slice(4)],
+            ((destination, tail) =>
+              ((currentVal, destinationVal, tailVal) =>
+                (cups[destination] = currentVal) &&
+                (cups[current] = tailVal) &&
+                (cups[tail] = destinationVal) &&
+                cups
+              )(
+                cups[current],
+                cups[destination],
+                cups[tail],
+              )
+            )(
+              [current - 1, current - 2, current - 3, current - 4]
+                .map(n => n < 1 ? n + cupsList.length : n)
+                .find(n =>
+                  n !== cups[current] &&
+                  n !== cups[cups[current]] &&
+                  n !== cups[cups[cups[current]]]
+                ),
+              cups[cups[cups[current]]],
+            )
           ),
-          cups
+          {
+            cups: linkedCups,
+            current: cupsList[0],
+          },
         )
+        .cups
     )
   )(
-    cups => [...cups.slice(1), cups[0]]
+    cupsList
+      .reduce(
+        (linkedCups, n, i, cupsList) =>
+          (linkedCups[n] = cupsList[(i + 1) % cupsList.length]) && linkedCups,
+        []
+      )
   )
 )(
   '158937462'
