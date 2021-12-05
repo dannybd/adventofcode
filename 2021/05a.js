97155e03d0dc357d474841630c8db2971e633b1a
dannybd <dannybd@mit.edu>
2021-12-05 05:57:51 +0000

    Day 5: shortened with single drawLine method

--- 2021/05a.js
+++ 2021/05a.js
@@ -1,11 +1,14 @@
-((lines, grid, drawVertical, drawDiagonal) =>
+((lines, grid) =>
   lines
     // Remove diagonals (comment this out for 5b's solution)
     .filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2)
-    .forEach(line =>
-      line[0][0] === line[1][0]
-        ? drawVertical(grid, line)
-        : drawDiagonal(grid, line)
+    .forEach(([[x1, y1], [x2, y2]]) =>
+      [...Array((x2 - x1 || y2 - y1) + 1).keys()]
+        .map(t => [
+          x1 + t * Math.sign(x2 - x1),
+          y1 + t * Math.sign(y2 - y1),
+        ])
+        .forEach(([x, y]) => grid[y][x]++),
     ) ||
   grid.flat().filter(n => n > 1).length
 )(
@@ -19,14 +22,4 @@
     ),
   // grid
   [...Array(1000).keys()].map(_ => Array(1000).fill(0)),
-  // drawVertical
-  (grid, [[x1, y1], [_, y2]]) =>
-    [...Array(y2 - y1 + 1).keys()]
-      .map(dy => [x1, dy + y1])
-      .forEach(([x, y]) => grid[y][x]++),
-  // drawDiagonal
-  (grid, [[x1, y1], [x2, y2]]) =>
-    [...Array(x2 - x1 + 1).keys()]
-      .map(dx => [dx + x1, Math.sign(y2 - y1) * dx + y1])
-      .forEach(([x, y]) => grid[y][x]++),
 )
